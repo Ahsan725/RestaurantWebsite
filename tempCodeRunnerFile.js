ba12d6831b5078c91ffda7e9cd7b1bf191dd0ae7
Ahsan725 <125620778+Ahsan725@users.noreply.github.com>
2023-08-01 18:53:54 -0400

    Delete tempCodeRunnerFile.js

--- tempCodeRunnerFile.js
+++
@@ -1,8 +0,0 @@
-function blank3() {
-  var x = [3, 6, 2, 89.45, 56, 23, 22, 21, 56, 1];
-  x.sort(function (a, b) {
-    return a - b;
-  });
-  //this will serve kind of like a comparator. Kind of like bubble sort.
-  document.getElementById("hp").innerHTML = x;
-}
